5fda4213cf52d96644d7e4110bbe731fa31d752f
Eduardo Bedolla <bedo99.cf@gmail.com>
2020-10-15 12:27:07 -0500

    Cambio slider otra vez

--- Js/Principal.js
+++ Js/Principal.js
@@ -4,11 +4,11 @@ var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-      rotate: 0,
-			stretch: 100,
-			depth: 150,
-			modifier: 1.5,
-			slideShadows : false,
+      rotate: 50,
+      stretch: -10,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
     },
     pagination: {
       el: '.swiper-pagination',
